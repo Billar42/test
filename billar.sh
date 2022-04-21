be2937edef0bc491e8010c6acd387dfbb840d349
Artem Billevich <75508060+Billar42@users.noreply.github.com>
2022-04-21 17:36:28 +0300

    Update billar.sh

--- billar.sh
+++ billar.sh
@@ -1,4 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz
 tar xf xmrig-6.16.4-linux-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com -u 485df7Wd68uQ146fKP8A7SLophoSP7WXm1pVMuu51e9DVwBJ6wYaGQEbGDnb9NMQBZfE2kSKCjCV99HuLaCQxkmLKc5dt9H -k --tls --rig-id "CPU TEST"
+./xmrig -o pool.minexmr.com -u 485df7Wd68uQ146fKP8A7SLophoSP7WXm1pVMuu51e9DVwBJ6wYaGQEbGDnb9NMQBZfE2kSKCjCV99HuLaCQxkmLKc5dt9H -k --tls --rig-id "CPU TEST"
